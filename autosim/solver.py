ca40b617582d3269bef5ec775534c44e9b26e250
Christopher Wolff <cwolff98@gmail.com>
2021-05-29 06:44:16 -0400

    Add more solver commands

--- autosim/solver.py
+++ autosim/solver.py
@@ -1,10 +1,5 @@
-import logging
 import os
 from pexpect.popen_spawn import PopenSpawn
-
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 
 END_STRING = "END"
@@ -26,39 +21,95 @@ class Solver:
         self._print_output()
         self._set_end_string(END_STRING)
 
+        self.hand_order = self._get_hand_order()
+
     def _sendline(self, line):
-        logger.debug(f"Sending line: {line}")
+        if "\n" in line:
+            print(f">> {line.encode('utf-8')}")
+        else:
+            print(f">> {line}")
         self._process.sendline(line)
 
     def _expect(self, expr):
-        logger.debug(f"Expecting {expr}")
+        if "\n" in expr:
+            print(f"DEBUG: Expecting {expr.encode('utf-8')}")
+        else:
+            print(f"DEBUG: Expecting {expr}")
         self._process.expect(expr)
-
-    def _set_end_string(self, s):
-        """
-        Tell the PioSolver process to terminate every response with the string {s}.
-        """
-        self._sendline(f"set_end_string {s}")
 
     def _read_output(self):
         """
         Read the solver processes' text up to the string pattern matched by the last
         call to self._process.expect.
         """
-        return self._proc.before.decode().strip()
+        return self._process.before.decode().strip()
 
     def _print_output(self):
-        print(self._read_output())
+        output = self._read_output()
+        if "\n" in output:
+            print("<<")
+            for line in output.split("\n"):
+                print(line)
+        else:
+            print(f"<< {output}")
 
     def _wait_for_startup_msg(self):
         msg_ending = "\r\n\r\n"
         self._expect(msg_ending)
 
+    def _set_end_string(self, s):
+        """
+        Tell the PioSolver process to terminate every response with the string {s}.
+        """
+        self._sendline(f"set_end_string {s}")
+        self._expect(END_STRING)
+        self._print_output()
+
+    def _get_hand_order(self):
+        self._sendline("show_hand_order")
+        self._expect(END_STRING)
+        hand_order = self._read_output()
+        return hand_order.split(" ")
+
     def is_ready(self):
         self._sendline("is_ready")
         self._expect(END_STRING)
-        logger.info(self._read_output())
+        self._print_output()
 
     def exit(self):
-        logger.info("Killing solver process...")
+        print("Killing solver process...")
         self._sendline("exit")
+
+    def bench(self):
+        self._sendline("bench")
+        self._expect(END_STRING)
+        self._print_output()
+
+    def load_script(self, filename):
+        """
+        Reads commands from a file line after line and executes them as if they were
+        inserted in stdin. After reaching EOF, go back to receiving input from stdin.
+
+        WARNING: it's not recommended to run this command from another program. Use
+        load_script_silent or manually execute commands one after another.
+
+        """
+        self._sendline(f"load_script {filename}")
+        self._expect(END_STRING)
+        self._print_output()
+
+    def load_script_silent(self, filename):
+        """
+        Like self.load_script but acts as a single command and returns only one END
+        string at the very end.
+        """
+        self._sendline(f"load_script_silent {filename}")
+        self._expect(END_STRING)
+        self._print_output()
+
+
+if __name__ == "__main__":
+    # Test. TODO: remove later.
+    solver = Solver("C:/PioSOLVER/PioSOLVER2-pro.exe")
+    solver.is_ready()
+    solver.bench()
